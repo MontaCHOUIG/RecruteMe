0f2683837b281601432ff36ea7b87111df06e6fe
Montassar Chouiguir <montachouig@gmail.com>
2025-02-07 23:25:56 +0100

    webhook svix fixedd

--- server/server.js
+++ server/server.js
@@ -39,9 +39,7 @@ app.get("/", (req, res) => {
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
-app.post("/webhooks", (req, res) => {
-  console.log(req.body);
-});
+app.post("/webhooks", clerkWebhook.clerkWebhook);
 const PORT = process.env.PORT || 3000;
 
 // The error handler must be registered before any other error middleware and after all controllers
